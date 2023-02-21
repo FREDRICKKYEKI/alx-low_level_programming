0ec0da06c114488d66dd7656bb7db9c3d256bb12
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-02-21 04:27:30 -0800

    correction 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include "alx.h"
+#include "main.h"
 /**
  * _islower - Shows 1 if the input is a
  * lowercase character. Another cases, shows
@@ -19,17 +19,4 @@ int _islower(int c)
 		return (0);
 	}
 	_putchar('\n');
-}
-int main(void)
-{
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
 }
