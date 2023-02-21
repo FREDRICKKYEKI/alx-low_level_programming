f50470ca9e3c5b6c915d2ab49c214e25f5a2e5f7
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-02-21 04:29:35 -0800

    correction4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "alx.h"
+#include "main.h"
 
 /**
  * _isalpha - Shows 1 if the input is a
@@ -19,20 +19,4 @@ int _isalpha(int c)
 		return (0);
 	}
 	_putchar('\n');
-}
-
-int main(void)
-{
-    int r;
-
-    r = _isalpha('H');
-    _putchar(r + '0');
-    r = _isalpha('o');
-    _putchar(r + '0');
-    r = _isalpha(108);
-    _putchar(r + '0');
-    r = _isalpha(';');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
 }
