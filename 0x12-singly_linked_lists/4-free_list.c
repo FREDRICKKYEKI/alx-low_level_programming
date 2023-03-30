71aa19fb854fddde8752d64eb24867f05ce384b2
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-03-30 18:27:13 +0300

    4-free-list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,12 +7,11 @@
 void free_list(list_t *head)
 {
 	list_t *current = head;
-	list_t *next;
 
 	while (current != NULL)
 	{
-		next = current->next;
+		head = head->next;
+		free(current->str);
 		free(current);
-		current = next;
 	}
 }
