61bed7b8f304e926966d43e41efd15f56e5d1dd3
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-03-30 16:16:40 +0300

    0-printlist

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements in a list.
+ * @h: Pointer to the header of the linked list.
+ * 
+ * Return: number of elements in the list;
+*/
+
+size_t print_list(const list_t *h)
+{
+    size_t nelem;
+
+    nelem = 0;
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+        
+        h = h->next;
+        nelem++;
+    }
+
+    return (nelem);
+}
