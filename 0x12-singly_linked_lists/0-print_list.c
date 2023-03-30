1e38a9642bb7dcb8b295892e54ff17d7c865f638
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-03-30 06:24:26 -0700

    [fix]: 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,19 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t nelem;
+	size_t nelem;
 
-    nelem = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        
-        h = h->next;
-        nelem++;
-    }
-
-    return (nelem);
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n")
+		else
+			printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	nelem++;
+	}
+	return (nelem);
 }
