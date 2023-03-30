6f3a0f485966f9e52f3a4bf535e8f01316e72a52
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-03-30 17:09:07 +0300

    1-list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: singly linked lists.
+ * 
+ * Return: number of elements
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t nelem;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			nelem++;
+		}
+	}
+	printf("%d elements", nelem);
+	return (nelem);
+}
