a5f59a2700a67a28be63a925cae2da12c5f6ddfb
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-04-04 19:18:36 +0300

    betty comply

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,13 +7,6 @@
  * @index: index of the list where the new node should be deleted.
  * Return: 1 if succeeded or -1 if it fails.
  */
-/**
- * delete_nodeint_at_index -  deletes the node
- * at a given index of a listint_t.
- * @head: head of linked list.
- * @index: index of the list where the new node should be deleted.
- * Return: 1 if succeeded or -1 if it fails.
- */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int count;
