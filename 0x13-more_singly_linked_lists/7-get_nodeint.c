73d7a13c34b9f56d4f1a6231801dfa1170598ec4
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-04-04 19:48:13 +0300

    betty comply

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,13 +9,11 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int count;
+	unsigned int i;
 
-	count = 0;
-	while (head != NULL && count < index)
+	for (i = 0; i < index && head != NULL; i++)
 	{
 		head = head->next;
-		count++;
 	}
 
 	return (head);
