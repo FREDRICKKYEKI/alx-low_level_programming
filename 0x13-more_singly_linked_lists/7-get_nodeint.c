81e4f6fde940e8562151ae89c8045763c6605697
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-04-04 19:46:08 +0300

    betty comply

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - adds a new node at the beginning of a listint_t
+ * get_nodeint_at_index - adds a new node at the beginning of a listint_t.
  * @head: head of linked list.
- * @index: index of the node starting at 0
+ * @index: index of the node starting at 0.
+ * 
  * Return: nth node or NULL if index does not exist.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -11,15 +12,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	int count;
 
 	count = 0;
-	while (head != NULL)
+	while (head != NULL && count < index)
 	{
 		head = head->next;
 		count++;
-		if (count == index)
-		break;
 	}
-	if (index > count || index <= 0)
-	return (NULL);
 
 	return (head);
 }
