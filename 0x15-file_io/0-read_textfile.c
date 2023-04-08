92f6e28c49dd6ff05202fc570cb1a4534345289c
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-04-08 09:22:33 +0300

    correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -36,7 +36,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	bytes_written = write(STDOOUT_FILENO, buf, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		close(fd);
