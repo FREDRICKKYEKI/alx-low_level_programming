5447f1307e4e7fc97197add0002f815a72ff812f
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-04-08 09:51:51 +0300

    create_file4

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,25 +11,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int bytes_written;
 	int fd;
+	int nofletters;
+	int bytes_written;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 		text_content = "";
 
-	bytes_written = write(fd, text_content, strlen(text_content));
+	for (nofletters = 0; text_content[nofletters]; nofletters++)
+		;
+
+	bytes_written = write(fd, text_content, nofletters);
 
 	if (bytes_written == -1)
 		return (-1);
-
 
 	close(fd);
 
