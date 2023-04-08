351db21916a964911f4d3d313e1ca0e0e38493c3
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-04-08 09:40:22 +0300

    create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#define PERM_RW (0600)
+
+/**
+ * create_file - creates a file with the specified name and writes the given
+ *text content to it
+ *
+ * @filename: the name of the file to create
+ * @text_content: the text content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t bytes_written;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, PERM_RW);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
